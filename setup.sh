066ffc76f023e84d711eb4810f23b37d6b3797ae
shingo <shingo@shingo-mac.local>
2011-05-08 22:20:09 +0900

    modify details

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 
 cp config.php.sample config.php
 chmod 777 ./application/cache
+mkdir -p ./application/cache/templates_c
 chmod 777 ./application/cache/templates_c
+mkdir -p ./img/upload/image
 chmod 777 ./img/upload/image
